b34c11e33fbbaa324001dfde43df9b5613427548
AryanJ18 <18aryanj2006@gmail.com>
2026-07-16 14:44:23 +0530

    Workflow for checking the working of backend

---
+++ backend/test.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+from backend.app import app
+
+client = TestClient(app)
+
+def test_predict():
+    response = client.post("/predict",
+        json={
+            "start_area": "Rohini",
+            "end_area": "Dwarka",
+            "distance_km": 10,
+            "time_of_day": "Morning",
+            "day_of_week": "Weekday",
+            "weather_condition": "Clear",
+            "road_type": "Main Road"
+        }
+    )
+
+    assert response.status_code == 200
+    assert "predicted_travel_time_minutes" in response.json()
